51eba9f7fbcde9b4259eb431244f2b6fb5f868ec
seo <seotownsend@icloud.com>
2015-03-15 16:14:20 -0400

    Finished library w/ unit tests

--- lib/iarrogant.rb
+++ lib/iarrogant.rb
@@ -2,10 +2,18 @@ require "iarrogant/version"
 require 'plist'
 
 module Iarrogant
-  #Convert from normal .Entitlement file to a proper .xcent
+  #Convert from normal .Entitlement file to a proper .xcent, returns a string
   def self.convert_ent_to_xcent(entitlement_path:, team_identifier:, bundle_id:, aps_environment:)
+    #Decodes into a Hash
     r = Plist::parse_xml(entitlement_path)
     raise "Could not locate entitlement at path #{entitlement_path.inspect}" unless r
-    r
+
+    #Set team-identifier
+    r['com.apple.developer.team-identifier'] = team_identifier
+    r['aps-environment'] = aps_environment
+    r['application-identifier'] = "#{team_identifier}.#{bundle_id}"
+    r['get-task-allow'] = true
+
+    rtrue.to_plist
   end
 end
--- spec/entitlement_spec.rb
+++ spec/entitlement_spec.rb
@@ -1,5 +1,7 @@
 #require 'spec_helper'
 require './lib/iarrogant.rb'
+require 'tempfile'
+require 'securerandom'
 
 RSpec.describe "convert_ent_to_xcent" do
   it "Dosen't throw a name exception" do
@@ -10,8 +12,67 @@ RSpec.describe "convert_ent_to_xcent" do
     end
   end
 
-  it "Returns something with a file" do
+  it "Returns a string with a real entitlements file path" do
     res = Iarrogant.convert_ent_to_xcent entitlement_path:"./spec/resources/Test.entitlements", team_identifier:"identifier", bundle_id:"bundle_id", aps_environment:"aps_environment"
-    res.should_not eq(nil)
+    res.class.should eq(String)
+  end
+
+  it "Returns a plistable string with a real entitlement file path" do
+    res = Iarrogant.convert_ent_to_xcent entitlement_path:"./spec/resources/Test.entitlements", team_identifier:"identifier", bundle_id:"bundle_id", aps_environment:"aps_environment"
+
+    f = Tempfile.new('iarrogant')
+    f.write(res)
+    f.close
+    k = Plist::parse_xml(f.path)
+    k.class.should eq(Hash)
+  end
+
+  it "Returns a plistable string that contains the correct team identifier" do
+    team_identifier = "#{SecureRandom.hex}"
+    res = Iarrogant.convert_ent_to_xcent entitlement_path:"./spec/resources/Test.entitlements", team_identifier:team_identifier, bundle_id:"bundle_id", aps_environment:"aps_environment"
+
+    f = Tempfile.new('iarrogant')
+    f.write(res)
+    f.close
+    k = Plist::parse_xml(f.path)
+    k["com.apple.developer.team-identifier"].should eq(team_identifier)
+  end
+
+  it "Returns a plistable string that contains the correct aps environment" do
+    team_identifier = "#{SecureRandom.hex}"
+    aps_environment = 'production'
+    res = Iarrogant.convert_ent_to_xcent entitlement_path:"./spec/resources/Test.entitlements", team_identifier:team_identifier, bundle_id:"bundle_id", aps_environment:aps_environment
+
+    f = Tempfile.new('iarrogant')
+    f.write(res)
+    f.close
+    k = Plist::parse_xml(f.path)
+    k["aps-environment"].should eq(aps_environment)
+  end
+
+  it "Returns a plistable string that contains the correct application identifier" do
+    team_identifier = "#{SecureRandom.hex}"
+    aps_environment = 'production'
+    bundle_id = "com.#{SecureRandom.hex}"
+    res = Iarrogant.convert_ent_to_xcent entitlement_path:"./spec/resources/Test.entitlements", team_identifier:team_identifier, bundle_id:bundle_id, aps_environment:aps_environment
+
+    f = Tempfile.new('iarrogant')
+    f.write(res)
+    f.close
+    k = Plist::parse_xml(f.path)
+    k["application-identifier"].should eq("#{team_identifier}.#{bundle_id}")
+  end
+
+  it "Returns a plistable string that contains a true get-task-allow" do
+    team_identifier = "#{SecureRandom.hex}"
+    aps_environment = 'production'
+    bundle_id = "com.#{SecureRandom.hex}"
+    res = Iarrogant.convert_ent_to_xcent entitlement_path:"./spec/resources/Test.entitlements", team_identifier:team_identifier, bundle_id:bundle_id, aps_environment:aps_environment
+
+    f = Tempfile.new('iarrogant')
+    f.write(res)
+    f.close
+    k = Plist::parse_xml(f.path)
+    k["get-task-allow"].should eq(true)
   end
 end
