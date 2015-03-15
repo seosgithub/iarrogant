75c1098476f273131af14f6f2637830e73333fb8
seo <seotownsend@icloud.com>
2015-03-15 16:00:37 -0400

    Put a test entitlements file and added the first two real tests

--- lib/iarrogant.rb
+++ lib/iarrogant.rb
@@ -1,9 +1,11 @@
 require "iarrogant/version"
+require 'plist'
 
 module Iarrogant
   #Convert from normal .Entitlement file to a proper .xcent
-  def convert_ent_to_xcent(entitlement_path:, team_identifier:, bundle_id:, aps_environment:)
+  def self.convert_ent_to_xcent(entitlement_path:, team_identifier:, bundle_id:, aps_environment:)
     r = Plist::parse_xml(entitlement_path)
-    puts r
+    raise "Could not locate entitlement at path #{entitlement_path.inspect}" unless r
+    r
   end
 end
--- spec/entitlement_spec.rb
+++ spec/entitlement_spec.rb
@@ -1,7 +1,17 @@
 #require 'spec_helper'
+require './lib/iarrogant.rb'
 
-RSpec.describe "Fuck" do
-  it "fucks up" do
-    pending
+RSpec.describe "convert_ent_to_xcent" do
+  it "Dosen't throw a name exception" do
+    begin
+      Iarrogant.convert_ent_to_xcent entitlement_path:"path", team_identifier:"identifier", bundle_id:"bundle_id", aps_environment:"aps_environment"
+    rescue => e
+      e.class.should_not eq(NameError)
+    end
+  end
+
+  it "Returns something with a file" do
+    res = Iarrogant.convert_ent_to_xcent entitlement_path:"./spec/resources/Test.entitlements", team_identifier:"identifier", bundle_id:"bundle_id", aps_environment:"aps_environment"
+    res.should_not eq(nil)
   end
 end
